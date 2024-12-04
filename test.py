7fa6fa1f1e9d23ce7298eda6f179067562d15e72
T-P2 <663070310060@npu.ac.th>
2024-12-04 14:59:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 print ("Hello World")
 print ("Aekasit")
-<<<<<<< HEAD
-print ("TTT")
-print ("RRR")
-=======
 print ("Aekasit")
 print ("Aekasit")
 print ("Aekasit")
@@ -11,4 +7,4 @@ print ("Aekasit")
 print ("Aekasit")
 print ("Aekasit")
 print ("Aekasit")
->>>>>>> 60a594a1a02bab4b19417dc7abde21f39cb121ff
+print ("Aekasit")
